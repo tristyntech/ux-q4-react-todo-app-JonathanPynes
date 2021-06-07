60f2ca7d32e69aba33220c8d8fc071cb7bd3a4dc
Jonathan Pynes <jonpynes@gmail.com>
2021-06-07 14:47:49 -0400

    Coming soon! Real Reactions to React!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>I am a React Application!</div>;
+  let myVariable = "I am a variable!";
+  let myOtherVariable = "I am variable #2";
+  let purpleDino = "I am a purple Dino";
+  let imgMaybe =
+    "Jake or who ever is grading this... can I use these variable to switch between pics after a click event?";
+  return (
+    <div>
+      <h1>I am a React Application!</h1>;
+      <p>This is a paragraph with a variables: {myVariable}</p>
+      <p>
+        When I was a boy we had to use vanilla JS: {myOtherVariable} ,{" "}
+        {purpleDino}
+      </p>
+      <p>{imgMaybe}</p>
+    </div>
+  );
 }
 
 export default App;
