38d89e6d943c9f5ca2c71c76b8fbc9f10ea1d01d
Jonathan Pynes <jonpynes@gmail.com>
2021-06-26 17:03:32 -0400

    added some styling becuase UX

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,25 +4,38 @@ import styled from "styled-components";
 const TodoItem = (props) => {
   const { title, description, isCompleted, id } = props.todo;
   return (
-    <li className="todo_item">
+    <TodoItemsLi>
       <p>
         <BoldText>Title: </BoldText> {title}
       </p>
       <p>
         <BoldText>Description: </BoldText> {description}
       </p>
+      <label>Check when done!</label>
       <input
         type="checkbox"
         checked={isCompleted}
         onChange={() => props.handleComplete(id)}
       />
-      <button onClick={() => props.handleDelete(id)}>Delete</button>
-    </li>
+      <DeleteButton onClick={() => props.handleDelete(id)}>Delete</DeleteButton>
+    </TodoItemsLi>
   );
 };
 
+const TodoItemsLi = styled.li`
+  margin-top: 2rem;
+  padding: 1rem;
+  border: solid;
+  background-color: lightyellow;
+`;
 const BoldText = styled.p`
   font-weight: bold;
+`;
+
+const DeleteButton = styled.button`
+  margin-left: 4rem;
+  background-color: darkred;
+  color: white;
 `;
 
 export default TodoItem;
